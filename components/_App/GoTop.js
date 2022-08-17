81de134cda7473d9dd3bf932d74c850a0d3803ac
13prashant <pacific1392@gmail.com>
2022-08-17 22:40:31 +0530

    added layout

---
+++ components/_App/GoTop.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+const GoTop = ({ scrollStepInPx, delayInMs }) => {
+  const [thePosition, setThePosition] = useState(false);
+  const timeoutRef = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener('scroll', () => {
+      if (window.scrollY > 170) {
+        setThePosition(true);
+      } else {
+        setThePosition(false);
+      }
+    });
+  }, []);
+
+  const onScrollStep = () => {
+    if (window.pageYOffset === 0) {
+      clearInterval(timeoutRef.current);
+    }
+    window.scroll(0, window.pageYOffset - scrollStepInPx);
+  };
+
+  const scrollToTop = () => {
+    timeoutRef.current = setInterval(onScrollStep, delayInMs);
+  };
+
+  const renderGoTopIcon = () => {
+    return (
+      <div
+        className={`go-top ${thePosition ? 'active' : ''}`}
+        onClick={scrollToTop}
+      >
+        <i className="bx bx-up-arrow-alt"></i>
+      </div>
+    );
+  };
+
+  return <>{renderGoTopIcon()}</>;
+};
+
+export default GoTop;
