8ec99aa8ecf7ed459563db66bb823f1ace8b7a84
13prashant <pacific1392@gmail.com>
2022-08-20 17:17:32 +0530

    replaced colors with variables in logo component

--- components/_App/Logo.js
+++ components/_App/Logo.js
@@ -1,6 +1,12 @@
 const Logo = () => {
   return (
-    <h3 style={{ color: 'white', textShadow: '2px 2px #fe4c1c', margin: '0' }}>
+    <h3
+      style={{
+        color: 'var(--whiteColor)',
+        textShadow: '2px 2px var(--mainColor)',
+        margin: '0',
+      }}
+    >
       SONGIRE
     </h3>
   );
