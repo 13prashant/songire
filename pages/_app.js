94e0a1f94b7e8ab32125b32ff3cb342417910d31
13prashant <pacific1392@gmail.com>
2022-12-30 21:52:50 +0530

    added google-tag-manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import '../public/css/bootstrap.min.css';
 import '../public/css/animate.min.css';
 import '../public/css/boxicons.min.css';
@@ -10,8 +11,12 @@ import '../public/css/style.scss';
 import '../public/css/responsive.scss';
 
 import Layout from '../components/_App/Layout';
+import TagManager from 'react-gtm-module';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: process.env.NEXT_PUBLIC_GTM_ID });
+  }, []);
   return (
     <Layout>
       <Component {...pageProps} />
