c9a4206e9cf3dc0d6dc73f03f3ee2f327423dde7
13prashant <pacific1392@gmail.com>
2023-02-08 21:45:03 +0530

    changed log & favicon

--- components/_App/Logo.js
+++ components/_App/Logo.js
@@ -1,15 +1,5 @@
 const Logo = () => {
-  return (
-    <h3
-      style={{
-        color: 'var(--whiteColor)',
-        textShadow: '2px 2px var(--mainColor)',
-        margin: '0',
-      }}
-    >
-      SONGIRE
-    </h3>
-  );
+  return <img src="/images/logo-light.png" width={'150px'} />;
 };
 
 export default Logo;
